d86bae950de41c39038e965b2a72dff5f90bbaa8
leithdm@gmail.com <leithdm@gmail.com>
2020-05-07 11:44:42 +0100

    1. Login and Signup functionality added

--- ParseStarterProject/src/main/java/com/parse/starter/MainActivity.java
+++ ParseStarterProject/src/main/java/com/parse/starter/MainActivity.java
@@ -13,8 +13,10 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Switch;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.parse.FindCallback;
@@ -33,8 +35,40 @@ import com.parse.SignUpCallback;
 import java.util.List;
 
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
+    Boolean _signUpModeActive = true;
+    TextView _loginTextView;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        _loginTextView = (TextView) findViewById(R.id.loginTextView);
+        _loginTextView.setOnClickListener(this);
+
+        ParseAnalytics.trackAppOpenedInBackground(getIntent());
+    }
+
+
+    @Override
+    public void onClick(View v) {
+        if (v.getId() == R.id.loginTextView) {
+            Button signupButton = (Button) findViewById(R.id.signupButton);
+
+            if (_signUpModeActive) {
+                _signUpModeActive = false;
+                signupButton.setText("Login");
+                _loginTextView.setText("or, Sign Up");
+            } else {
+                _signUpModeActive = true;
+                signupButton.setText("Sign up");
+                _loginTextView.setText("or, Login");
+            }
+        }
+    }
 
     public void signUpClicked(View view) {
         EditText username = (EditText) findViewById(R.id.userNameEditText);
@@ -43,33 +77,35 @@ public class MainActivity extends AppCompatActivity {
         if ((username.getText().toString().matches("")) || (password.getText().toString().matches(""))) {
             Toast.makeText(this, "A username and a password are required", Toast.LENGTH_SHORT).show();
         } else {
-            ParseUser user = new ParseUser();
-            user.setUsername(username.getText().toString());
-            user.setPassword(password.getText().toString());
+            if (_signUpModeActive) {
+                //signup
+                ParseUser user = new ParseUser();
+                user.setUsername(username.getText().toString());
+                user.setPassword(password.getText().toString());
 
-            user.signUpInBackground(new SignUpCallback() {
-                @Override
-                public void done(ParseException e) {
-                    if (e == null) {
-                        Log.i("Signup status", "Successful");
-                    } else {
-                        Toast.makeText(MainActivity.this, e.getMessage(), Toast.LENGTH_SHORT).show();
+                user.signUpInBackground(new SignUpCallback() {
+                    @Override
+                    public void done(ParseException e) {
+                        if (e == null) {
+                            Log.i("Signup status", "Successful");
+                        } else {
+                            Toast.makeText(MainActivity.this, e.getMessage(), Toast.LENGTH_SHORT).show();
+                        }
                     }
-                }
-            });
+                });
+            } else {
+                //login
+                ParseUser.logInInBackground(username.getText().toString(), password.getText().toString(), new LogInCallback() {
+                    @Override
+                    public void done(ParseUser user, ParseException e) {
+                        if (user != null) {
+                            Log.i("Login status", "successful");
+                        } else {
+                            Toast.makeText(MainActivity.this, e.getMessage(), Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+            }
         }
-
-
     }
-
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
-
-        ParseAnalytics.trackAppOpenedInBackground(getIntent());
-    }
-
 }
